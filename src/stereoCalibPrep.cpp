8fcdac70ad4099a371cc0c025a61f0aaf6d063e2
Michał Orynicz <morynicz@gmail.com>
2013-11-11 12:10:09 +0100

    Fix broken include paths

--- src/stereoCalibPrep.cpp
+++ src/stereoCalibPrep.cpp
@@ -6,9 +6,9 @@
  */
 
 #include <boost/program_options.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <string>
 #include <iostream>
